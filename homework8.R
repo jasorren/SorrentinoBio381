6eb22ce0b7ec719f4398a487b21e961fbedf7f2a
jasorren <jasorren@uvm.edu>
2023-03-29 01:39:31 -0400

    add homework 8

---
+++ homework8.R
@@ -0,0 +1,22 @@
+treatment1Data <- round(abs(rnorm(n=150,mean=2,sd=1)))
+treatment2Data <- round(abs(rnorm(n=150,mean=2.19,sd=15)))
+
+treatment1Dataframe <- data.frame(1:150,treatment1Data)
+names(treatment1Dataframe) <- list("Treatment","number_of_slugs")
+treatment1Dataframe["Treatment"][treatment1Dataframe["Treatment"] >= 1] <- "treatment_1"
+
+treatment2Dataframe <- data.frame(1:150,treatment2Data)
+names(treatment2Dataframe) <- list("Treatment","number_of_slugs")
+treatment2Dataframe["Treatment"][treatment2Dataframe["Treatment"] >= 1] <- "treatment_2"
+
+fullDataframe <- rbind(treatment1Dataframe,treatment2Dataframe)
+
+fullDataframe$Treatment <- as.factor(fullDataframe$Treatment)
+
+t_test <- t.test(fullDataframe$number_of_slugs~fullDataframe$Treatment)
+t_test
+
+library(ggplot2)
+plot1 <- ggplot(fullDataframe,aes(x=Treatment,y=number_of_slugs))+geom_boxplot()
+print(plot1)
+
